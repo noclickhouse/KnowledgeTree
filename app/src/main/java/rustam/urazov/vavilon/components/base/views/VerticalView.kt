1c6d97b39d5a02d25db33b82db84e4f4fafb191e
askdh-b <rustpy2703@gmail.com>
2023-03-09 23:41:26 +0600

    vertical view centered horizontally

--- app/src/main/java/rustam/urazov/vavilon/components/base/views/VerticalView.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/base/views/VerticalView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -23,6 +24,6 @@ fun VerticalView(
         shape = shape,
         colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor)
     ) {
-        Column(content = content)
+        Column(horizontalAlignment = Alignment.CenterHorizontally, content = content)
     }
 }
