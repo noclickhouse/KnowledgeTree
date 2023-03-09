b557e74456ab6432065902bd44c9ab487cfc0d26
askdh-b <rustpy2703@gmail.com>
2023-03-09 22:36:15 +0600

    add vertical view

---
+++ app/src/main/java/rustam/urazov/vavilon/components/views/VerticalView.kt
@@ -0,0 +1,28 @@
+package rustam.urazov.vavilon.components.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+
+@Composable
+fun VerticalView(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color,
+    shape: Shape,
+    onClick: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor)
+    ) {
+        Column(content = content)
+    }
+}
