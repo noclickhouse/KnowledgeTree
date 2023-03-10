2831b98af13b24c65fcbaea08f3fe98e204be828
askdh-b <rustpy2703@gmail.com>
2023-03-10 11:15:27 +0600

    add constraints for tree view

--- app/src/main/java/rustam/urazov/vavilon/components/complex/TreeView.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/TreeView.kt
@@ -1,7 +1,9 @@
 package rustam.urazov.vavilon.components.complex
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,27 +21,30 @@ import rustam.urazov.vavilon.components.base.views.VerticalView
 fun TreeView(
     onClick: () -> Unit
 ) {
-    VerticalView(
-        modifier = Modifier.fillMaxSize(),
-        backgroundColor = Color(0xed, 0x89, 0xcb),
-        shape = RoundedCornerShape(16.dp),
-        onClick = onClick
-    ) {
-        val color = Color(0x89, 0xed, 0xaa)
-        WideImage(
-            imageId = R.drawable.add_24,
-            contentDescription = "",
-            modifier = Modifier.background(
-                color = Color(0xdc, 0x89, 0xed),
-                shape = RoundedCornerShape(16.dp)
-            ),
-            colorFilter = ColorFilter.tint(color)
-        )
-        Text(
-            text = "asd",
-            color = color,
-            fontSize = 24.sp
-        )
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val height = maxHeight
+        VerticalView(
+            modifier = Modifier.fillMaxSize(),
+            backgroundColor = Color(0xed, 0x89, 0xcb),
+            shape = RoundedCornerShape(16.dp),
+            onClick = onClick
+        ) {
+            val color = Color(0x89, 0xed, 0xaa)
+            WideImage(
+                imageId = R.drawable.add_24,
+                contentDescription = "",
+                modifier = Modifier.height(height) .background(
+                    color = Color(0xdc, 0x89, 0xed),
+                    shape = RoundedCornerShape(16.dp)
+                ),
+                colorFilter = ColorFilter.tint(color)
+            )
+            Text(
+                text = "asd",
+                color = color,
+                fontSize = 24.sp
+            )
+        }
     }
 }
 
