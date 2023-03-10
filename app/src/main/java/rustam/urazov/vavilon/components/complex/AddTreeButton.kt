7005e411fe08ec3363578ad69553da33dbb45d90
askdh-b <rustpy2703@gmail.com>
2023-03-10 10:19:50 +0600

    increase font and image sizes

--- app/src/main/java/rustam/urazov/vavilon/components/complex/AddTreeButton.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/AddTreeButton.kt
@@ -2,6 +2,7 @@ package rustam.urazov.vavilon.components.complex
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import rustam.urazov.vavilon.R
 import rustam.urazov.vavilon.components.base.views.VerticalView
 
@@ -29,9 +31,14 @@ fun AddTreeButton(
         Image(
             painter = painterResource(id = R.drawable.add_24),
             contentDescription = stringResource(id = R.string.click_for_add_tree),
+            modifier = Modifier.size(48.dp),
             colorFilter = ColorFilter.tint(color)
         )
-        Text(text = stringResource(id = R.string.click_for_add_tree), color = color)
+        Text(
+            text = stringResource(id = R.string.click_for_add_tree),
+            color = color,
+            fontSize = 24.sp
+        )
     }
 }
 
