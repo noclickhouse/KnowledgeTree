9adeb9b7892c1df50facf1cf3dd878de7988bb9b
askdh-b <rustpy2703@gmail.com>
2023-03-10 13:03:50 +0600

    update tree view colors to theme colors

--- app/src/main/java/rustam/urazov/vavilon/components/complex/TreeView.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/TreeView.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -17,6 +17,7 @@ import rustam.urazov.vavilon.R
 import rustam.urazov.vavilon.components.base.images.WideImage
 import rustam.urazov.vavilon.components.base.views.VerticalView
 import rustam.urazov.vavilon.components.models.ContentPadding
+import rustam.urazov.vavilon.ui.theme.VavilonTheme
 
 @Composable
 fun TreeView(
@@ -26,7 +27,7 @@ fun TreeView(
         val height = maxHeight
         VerticalView(
             modifier = Modifier.fillMaxSize(),
-            backgroundColor = Color(0xed, 0x89, 0xcb),
+            backgroundColor = MaterialTheme.colors.primary,
             shape = RoundedCornerShape(16.dp),
             onClick = onClick,
             contentPadding = ContentPadding(
@@ -34,21 +35,20 @@ fun TreeView(
                 horizontal = 16.dp
             )
         ) {
-            val color = Color(0x89, 0xed, 0xaa)
             WideImage(
                 imageId = R.drawable.add_24,
                 contentDescription = "",
                 modifier = Modifier
                     .height(height - 136.dp)
                     .background(
-                        color = Color(0xdc, 0x89, 0xed),
+                        color = MaterialTheme.colors.primary,
                         shape = RoundedCornerShape(16.dp)
                     ),
-                colorFilter = ColorFilter.tint(color)
+                colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary)
             )
             Text(
                 text = "ASD",
-                color = color,
+                color = MaterialTheme.colors.secondary,
                 fontSize = 24.sp
             )
             ProgressBarWithIndicator(progress = 0.6f)
@@ -59,7 +59,9 @@ fun TreeView(
 @Composable
 @Preview
 fun TreeViewPreview() {
-    TreeView {
+    VavilonTheme {
+        TreeView {
 
+        }
     }
 }
