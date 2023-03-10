f646ad6563ebf8b7a8bf9a3a73e74832b7ac3b98
askdh-b <rustpy2703@gmail.com>
2023-03-10 23:42:00 +0600

    update trees list

--- app/src/main/java/rustam/urazov/vavilon/components/complex/TreesList.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/TreesList.kt
@@ -1,15 +1,26 @@
 package rustam.urazov.vavilon.components.complex
 
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import rustam.urazov.vavilon.components.models.TreeInfoModel
 import rustam.urazov.vavilon.ui.theme.VavilonTheme
 
 @Composable
-fun TreesList(trees: List<TreeInfo>) {
+fun TreesList(trees: List<TreeInfoModel>) {
     LazyRow {
         items(trees) { tree ->
+            TreeView(
+                treeName = tree.name,
+                treeImage = null,
+                treeContentDescription = ""
+            ) {
 
+            }
+            AddTreeButton {
+
+            }
         }
     }
 }
@@ -18,6 +29,10 @@ fun TreesList(trees: List<TreeInfo>) {
 @Preview
 fun TreesListPreview() {
     VavilonTheme {
-        TreesList()
+        TreesList(
+            trees = listOf(
+                TreeInfoModel("asd", "", 0.4f)
+            )
+        )
     }
 }
