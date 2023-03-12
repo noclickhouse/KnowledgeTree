cc5a34b97221c69256f7982b8aa61c216e79df61
askdh-b <rustpy2703@gmail.com>
2023-03-12 18:13:55 +0600

    add branch

---
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/Branch.kt
@@ -0,0 +1,27 @@
+package rustam.urazov.vavilon.components.complex
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import rustam.urazov.vavilon.components.base.buttons.WideButtonWithText
+import rustam.urazov.vavilon.ui.theme.VavilonTheme
+
+@Composable
+fun Branch(
+    text: String,
+    onClick: () -> Unit,
+) {
+    WideButtonWithText(
+        onClick = onClick,
+        text = text
+    )
+}
+
+@Composable
+@Preview
+fun BranchPreview() {
+    VavilonTheme {
+        Branch("dsa") {
+
+        }
+    }
+}
