b42e7f1cd300017a26c0b132de1d0011797e3044
askdh-b <rustpy2703@gmail.com>
2023-03-12 18:51:27 +0600

    align branches in tree center horizontally

--- app/src/main/java/rustam/urazov/vavilon/components/complex/TreeScreen.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/TreeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +23,8 @@ fun TreeScreen(
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(height - 60.dp)
+                    .height(height - 60.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 items(branches) { branch ->
                     Branch(text = branch) {
