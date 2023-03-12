5b29df7b9604acc2627b7689301924ca68211200
askdh-b <rustpy2703@gmail.com>
2023-03-12 18:55:34 +0600

    add pddings for branch

--- app/src/main/java/rustam/urazov/vavilon/components/complex/Branch.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/Branch.kt
@@ -1,7 +1,10 @@
 package rustam.urazov.vavilon.components.complex
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import rustam.urazov.vavilon.components.base.buttons.ButtonWithText
 import rustam.urazov.vavilon.ui.theme.VavilonTheme
 
@@ -12,6 +15,7 @@ fun Branch(
 ) {
     ButtonWithText(
         text = text,
+        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
         onClick = onClick
     )
 }
