551badafb4ab36e1b40ddd1668e5f9127859bcad
askdh-b <rustpy2703@gmail.com>
2023-03-12 16:26:38 +0600

    update add button

--- app/src/main/java/rustam/urazov/vavilon/components/complex/AddButton.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/AddButton.kt
@@ -1,20 +1,18 @@
 package rustam.urazov.vavilon.components.complex
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import rustam.urazov.vavilon.components.base.buttons.WideButton
+import rustam.urazov.vavilon.R
 import rustam.urazov.vavilon.components.base.buttons.WideButtonWithText
 
 @Composable
 fun AddButton() {
     WideButtonWithText(
         onClick = { },
-        enabled = true,
         shape = RoundedCornerShape(16.dp),
-        text = "Add"
+        textId = R.string.add
     )
 }
 
