eecd5368175680cb0a9717a66e5f8c3ebf8ee3e7
askdh-b <rustpy2703@gmail.com>
2023-03-12 17:35:01 +0600

    add tree screen

---
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/TreeScreen.kt
@@ -0,0 +1,34 @@
+package rustam.urazov.vavilon.components.complex
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TreeScreen() {
+    BoxWithConstraints {
+        val height = maxHeight
+        Column {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(height - 60.dp)
+            ) {
+
+            }
+            AddButton()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun TreeScreenPreview() {
+    TreeScreen()
+}
