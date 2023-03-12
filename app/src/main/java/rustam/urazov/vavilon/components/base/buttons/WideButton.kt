0e2d48f08329ea2fda2ab954b89a0030b1f22266
askdh-b <rustpy2703@gmail.com>
2023-03-12 16:33:51 +0600

    update wide button shapes and paddings

--- app/src/main/java/rustam/urazov/vavilon/components/base/buttons/WideButton.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/base/buttons/WideButton.kt
@@ -1,25 +1,43 @@
 package rustam.urazov.vavilon.components.base.buttons
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import rustam.urazov.vavilon.ui.theme.VavilonTheme
 
 @Composable
 fun WideButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    shape: Shape,
+    shape: Shape = RoundedCornerShape(16.dp),
     content: @Composable RowScope.() -> Unit
 ) {
     Button(
         onClick = onClick,
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth(),
         enabled = enabled,
         shape = shape,
+        contentPadding = PaddingValues(16.dp),
         content = content
     )
+}
+
+@Composable
+@Preview
+fun WideButtonPreview() {
+    VavilonTheme {
+        WideButton(
+            onClick = {  },
+            shape = RoundedCornerShape(16.dp)
+        ) {  }
+    }
 }
