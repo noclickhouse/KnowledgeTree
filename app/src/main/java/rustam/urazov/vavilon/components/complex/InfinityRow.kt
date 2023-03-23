10bac49249d85b138e2c9f3b3a548a0b526a94ff
askdh-b <rustpy2703@gmail.com>
2023-03-23 14:39:59 +0600

    fix infinity row

--- app/src/main/java/rustam/urazov/vavilon/components/complex/InfinityRow.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/InfinityRow.kt
@@ -1,19 +1,9 @@
 package rustam.urazov.vavilon.components.complex
 
-import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.onFocusedBoundsChanged
-import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.*
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import rustam.urazov.vavilon.ui.theme.VavilonTheme
 import rustam.urazov.vavilon.viewmodels.Branch
 
@@ -21,25 +11,27 @@ import rustam.urazov.vavilon.viewmodels.Branch
 fun InfinityRow(
     branches: List<Branch.BranchView>
 ) {
-    val widths = mutableListOf(0 to 0.dp)
+    Layout(
+        content = {
+            branches.forEach { branch ->
+                BranchItem(text = branch.title) {
 
-    branches.forEach { branch ->
-        val density = LocalDensity.current
-        BranchItem(
-            text = branch.title,
-            modifier = Modifier
-                .onGloballyPositioned {
-                    density.run {
-                        widths.add(branch.id to it.size.width.dp)
-                    }
                 }
-                .offset(x = widths.find { branch.previousId == it.first }?.second ?: widths[0].second)
-        ) {
-
+            }
+        },
+        modifier = Modifier,
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
         }
-        widths.forEach {
-            Log.e("asd", it.first.toString())
-            Log.e("asd", it.second.toString())
+
+        layout(constraints.minWidth, constraints.minHeight) {
+            var xPosition = 0
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = xPosition, y = 0)
+                xPosition += placeable.width
+            }
         }
     }
 }
@@ -51,7 +43,7 @@ fun InfinityRowPreview() {
         InfinityRow(
             listOf(
                 Branch.BranchView(1, "assdd", 0, 0, 0f),
-                Branch.BranchView(2, "dsdsd", 0, 1,0f)
+                Branch.BranchView(2, "dsdsd", 0, 1, 0f)
             )
         )
     }
--- app/src/main/java/rustam/urazov/vavilon/components/complex/ModernTree.kt
+++ app/src/main/java/rustam/urazov/vavilon/components/complex/ModernTree.kt
@@ -1,19 +1,11 @@
 package rustam.urazov.vavilon.components.complex
 
 import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import rustam.urazov.vavilon.ui.theme.VavilonTheme
@@ -36,14 +28,13 @@ fun ModernTree() {
             .width(10000.dp)
             .height(10000.dp)
     ) {
-        val branches: List<Branch> = listOf(
+        val branches = listOf(
             Branch.BranchView(1, "asd", 0, 0, 0f),
             Branch.BranchView(2, "sdf", 0, 0,0f),
             Branch.BranchView(3, "qwe", 1, 0, 0f),
-            Branch.LeafView(1, "dfg", false, 3),
-            Branch.LeafView(2, "fgh", false, 2)
+            Branch.BranchView(4, "fgh", 1, 0, 0f)
         )
-        ModernBranch(0, branches)
+        InfinityRow(branches)
     }
 }
 
